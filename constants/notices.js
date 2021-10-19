efffb41ee6689a396dd18a88d166e1a2533b3aca
mohd salim reza <56276017+salim-sbn@users.noreply.github.com>
2021-10-19 17:51:14 +0530

    Update notices.js
    Updated notice

--- constants/notices.js
+++ constants/notices.js
@@ -1,5 +1,5 @@
 export const allNotices  = [
-    {path:'notices/notice1.jpg',title:'Notice 1'},
-    {path:'notices/notice2.jpg',title:'Notice 2'},
+    {path:'notices/notice1.jpeg',title:'Examination Schedule'},
+    {path:'notices/notice2.jpeg',title:'News Paper Advertisement'},
     {path:'notices/Datesheet.pdf',title:'Date Sheet'},
 ]
