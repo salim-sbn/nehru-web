7767bc50a2730f297d3178b5734b4e2200713c90
mohd salim reza <56276017+salim-sbn@users.noreply.github.com>
2021-10-19 17:48:31 +0530

    Update notices.js
    Rename notice 1 and 2

--- constants/notices.js
+++ constants/notices.js
@@ -1,5 +1,5 @@
 export const allNotices  = [
-    {path:'notices/notice1.pdf',title:'Notice 1'},
-    {path:'notices/notice2.pdf',title:'Notice 2'},
+    {path:'notices/notice1.jpg',title:'Notice 1'},
+    {path:'notices/notice2.jpg',title:'Notice 2'},
     {path:'notices/Datesheet.pdf',title:'Date Sheet'},
 ]
