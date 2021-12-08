c643aa24eefb77feec49e1b9e58393694214b044
mohd salim reza <56276017+salim-sbn@users.noreply.github.com>
2021-12-08 23:05:33 +0530

    practical notice on notice section

--- constants/notices.js
+++ constants/notices.js
@@ -1,4 +1,5 @@
 export const allNotices  = [
+    {path:'notices/practical_nioit.pdf',title:'Practical & Internal Assesment notice'},
     {path:'notices/notice1.jpeg',title:'Examination Schedule'},
     {path:'notices/notice2.jpeg',title:'News Paper Advertisement'},
     {path:'notices/Datesheet.pdf',title:'Date Sheet'},
