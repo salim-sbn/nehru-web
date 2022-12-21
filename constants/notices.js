96bff05bd2a355ac7d41b09183a8917cd52d8380
mohd salim reza <56276017+salim-sbn@users.noreply.github.com>
2022-12-21 21:26:00 +0530

    Update notices.js
    Notices updated

--- constants/notices.js
+++ constants/notices.js
@@ -1,6 +1,10 @@
 export const allNotices  = [
-    {path:'notices/practical_nioit.pdf',title:'Practical & Internal Assesment notice'},
-    {path:'notices/notice1.jpeg',title:'Examination Schedule'},
-    {path:'notices/notice2.jpeg',title:'News Paper Advertisement'},
-    {path:'notices/Datesheet.pdf',title:'Date Sheet'},
+//     {path:'notices/practical_nioit.pdf',title:'Practical & Internal Assesment notice'},
+//     {path:'notices/notice1.jpeg',title:'Examination Schedule'},
+//     {path:'notices/notice2.jpeg',title:'News Paper Advertisement'},
+//     {path:'notices/Datesheet.pdf',title:'Date Sheet'},
+    {path:'notices/Readmission Voactional.pdf',title:'Readmission Vocational'},
+    {path:'notices/KRC Online Examination Form.pdf"',title:'KRC Online Examination Form'},
+    
+    
 ]
