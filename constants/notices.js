a172fdb62526d2350998e88b9f32c36bf6f0d2a5
mohd salim reza <56276017+salim-sbn@users.noreply.github.com>
2023-03-25 13:16:52 +0530

    Update notices.js
    Notice updated

--- constants/notices.js
+++ constants/notices.js
@@ -3,8 +3,10 @@ export const allNotices  = [
 //     {path:'notices/notice1.jpeg',title:'Examination Schedule'},
 //     {path:'notices/notice2.jpeg',title:'News Paper Advertisement'},
 //     {path:'notices/Datesheet.pdf',title:'Date Sheet'},
-    {path:'notices/Exam_notice.pdf',title:' Part 2 & Part 3 Exam schedule '},
     
+    {path:'notices//Registration notice nioit.pdf',title:' Registration notice 2021-22 enrolled students '},
+
+    {path:'notices/Exam_notice.pdf',title:' Part 2 & Part 3 Exam schedule '},
     {path:'notices/Readmission Voactional.pdf',title:'Readmission Vocational'},
     {path:'notices/krc_form.pdf',title:' KRC Online Examination Form '},
     
