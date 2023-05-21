4feb3881440dba91515b25c184a5c916a5b642ec
mohd salim reza <56276017+salim-sbn@users.noreply.github.com>
2023-05-21 10:51:14 +0530

    Update notices.js
    Notice updated

--- constants/notices.js
+++ constants/notices.js
@@ -4,14 +4,17 @@ export const allNotices  = [
 //     {path:'notices/notice2.jpeg',title:'News Paper Advertisement'},
 //     {path:'notices/Datesheet.pdf',title:'Date Sheet'},
     
-    {path:'notices/Blis.pdf',title:' BLIS 2021-22 enrolled students details '},
+//     {path:'notices/Blis.pdf',title:' BLIS 2021-22 enrolled students details '},
     
-    {path:'notices/registration_notice.pdf',title:' Registration notice 2021-22 enrolled students '},
+//     {path:'notices/registration_notice.pdf',title:' Registration notice 2021-22 enrolled students '},
 
-    {path:'notices/Exam_notice.pdf',title:' Part 2 & Part 3 Exam schedule '},
-    {path:'notices/Readmission Voactional.pdf',title:'Readmission Vocational'},
-    {path:'notices/krc_form.pdf',title:' KRC Online Examination Form '},
+//     {path:'notices/Exam_notice.pdf',title:' Part 2 & Part 3 Exam schedule '},
+//     {path:'notices/Readmission Voactional.pdf',title:'Readmission Vocational'},
+//     {path:'notices/krc_form.pdf',title:' KRC Online Examination Form '},
     
+    
+{path:'notices/notice1_21_May.pdf',title:' Notice for Online Examination Form '},    
+{path:'notices/notice2_May.pdf',title:' Schedule for Vocational Examinations- 2022'},    
         
     
 ]
